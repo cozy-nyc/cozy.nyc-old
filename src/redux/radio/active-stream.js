bf90c3bac9af7a85152dbc0dfae16dc957bccc63
rantahu <rantahu@gmail.com>
2018-05-26 13:31:21 -0400

    OMFG

--- src/redux/radio/active-stream.js
+++ src/redux/radio/active-stream.js
@@ -1,10 +1,10 @@
-export default function (){
+export default function () {
   return [
     {
       id: 1,
       name: 'music classics',
       slug: 'classics',
-      image: 'image.gif' ,
+      image: 'image.gif',
       preview: 'video.mp4',
       listeners: 10,
     },
