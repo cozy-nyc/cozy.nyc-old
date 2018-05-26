586fe99fc6a448503ed2c06630a5feac0aa358a6
rantahu <rantahu@gmail.com>
2018-05-26 02:08:40 -0400

    no more compile errors, they're still warnings and runtime errors

--- src/actions/boards/get-threads.jsx
+++ src/actions/boards/get-threads.jsx
@@ -1,13 +1,13 @@
 import api from '../../utils/api';
 
-export function fetchBoard() {
-   return function(dispatch) {
-     api.get('/board/1/')
-        .then((response) => {
-           dispatch({type: "FETCH_THREADS_FULFILLED", payload: response.data})
-        })
-        .catch((err) => {
-           dispatch({type: "FETCH_THREADS_ERROR", payload: err})
-        })
-    }
+export default function fetchBoard() {
+  return function (dispatch) {
+    api.get('/board/1/')
+      .then(response => {
+        dispatch({ type: 'FETCH_THREADS_FULFILLED', payload: response.data });
+      })
+      .catch(err => {
+        dispatch({ type: 'FETCH_THREADS_ERROR', payload: err });
+      });
+  };
 }
