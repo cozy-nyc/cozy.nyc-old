b5dae249f84e594849ce982e70e7daeedfd8c66c
rantahu <rantahu@gmail.com>
2018-07-08 20:25:52 -0400

    minor changes to profiles

--- src/components/profilebutton/profile-button.jsx
+++ src/components/profilebutton/profile-button.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -19,19 +19,25 @@ class ProfileButton extends Component {
   };
 
   render() {
+    const profileURL = '/u/' + this.props.user.username;
+
     if (this.props.auth.isLogin) {
       return (
         <div id="profile-button">
           <div id="user-profile-status">
-            <p>{this.props.user.username}</p>
+            <NavLink to={{ pathname: profileURL }}>
+              <p>{this.props.user.username}</p>
+            </NavLink>
             <p>something here!</p>
           </div>
           <div id="user-profile-avatar">
-            <img
-              className="profile-image"
-              src={this.props.user.id}
-              alt={this.props.user.username}
-            />
+            <NavLink to={{ pathname: profileURL }}>
+              <img
+                className="profile-image"
+                src="https://avatars3.githubusercontent.com/u/2263236?s=400&u=b08c40e215bdaf416f6fecc4016add2b3182f824&v=4"
+                alt={this.props.user.username}
+              />
+            </NavLink>
           </div>
           <div className="u-cf" />
         </div>
