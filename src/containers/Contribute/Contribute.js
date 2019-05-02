6a1057b1ca301412c26b5f8dbf950c121b488726
S1MB10T3 <s1mb10t3@pm.me>
2019-05-02 15:08:40 -0400

    fixed navbar css issues

--- src/containers/Contribute/Contribute.js
+++ src/containers/Contribute/Contribute.js
@@ -10,20 +10,28 @@ export default function Contribute() {
       <article>
         <h3>We need all the help we can get!</h3>
         <p>
-          cozy.nyc is still in development and this site will go under constant change.
-          It might even break time to time. Our first production build removes a lot
-          of features that are sort of half done so we can work on it some more before
-          we release to the public for testing. To keep track of these changes join
-          us on our <a href="https://discord.gg/3WSA2SG">discord</a> and check out
-          our git repo at <a href="https://github.com/cozy-nyc/cozy-nyc" alt="">github.com/cozy-nyc/cozy-nyc</a>
+          cozy.nyc is still in development and this site will go under constant change. It might even break time to
+          time. Our first production build removes a lot of features that are sort of half done so we can work on it
+          some more before we release to the public for testing. To keep track of these changes join us on our{' '}
+          <a href="https://discord.gg/3WSA2SG">discord</a> and check out our git repo at{' '}
+          <a href="https://github.com/cozy-nyc/cozy-nyc" alt="">
+            github.com/cozy-nyc/cozy-nyc
+          </a>
         </p>
         <p>
-          Our API can be found here at <a href="http://api.cozy.nyc" alt="">api.cozy.nyc</a> 
-          and it's repo here at <a href="https://github.com/cozy-nyc/cozy-nyc-backend" alt="">
-          github.com/cozy-nyc/cozy-nyc-backend </a>
+          Our API can be found here at{' '}
+          <a href="http://api.cozy.nyc" alt="">
+            api.cozy.nyc{' '}
+          </a>
+          and it's repo here at{' '}
+          <a href="https://github.com/cozy-nyc/cozy-nyc-backend" alt="">
+            github.com/cozy-nyc/cozy-nyc-backend{' '}
+          </a>
         </p>
         <br />
-        <p>Check out the <Link to="about">about page</Link> for our goals!</p>
+        <p>
+          Check out the <Link to="about">about page</Link> for our goals!
+        </p>
         <br />
         <p styles="text-align: right;">
           Stay toasty,
@@ -31,7 +39,6 @@ export default function Contribute() {
           S1MB10T3
         </p>
       </article>
-
     </div>
   );
 }
