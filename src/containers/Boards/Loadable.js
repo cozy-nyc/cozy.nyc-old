23d350d85a2d3db9f49adf3a3a5c0a6d3c4a91ea
S1MB10T3 <s1mb10t3@pm.me>
2019-07-15 15:58:36 -0400

    renamed classes and added discovery page

--- src/containers/Boards/Loadable.js
+++ src/containers/Boards/Loadable.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Loadable from 'react-loadable';
 
-const BoardsCategoriesLoadable = Loadable({
+const BoardsHome = Loadable({
   loader: () => import('./Categories' /* webpackChunkName: 'boards' */).then(module => module.default),
   loading: () => <div>Loading</div>
 });
 
-const BoardLoadable = Loadable({
+const Board = Loadable({
   loader: () => import('./Board' /* webpackChunkName: 'boards' */).then(module => module.default),
   loading: () => <div>Loading</div>
 });
 
-const BoardsThreadLoadable = Loadable({
+const Thread = Loadable({
   loader: () => import('./Thread' /* webpackChunkName: 'boards' */).then(module => module.default),
   loading: () => <div>Loading</div>
 });
 
-export BoardsCategoriesLoadable, BoardLoadable, BoardsThreadLoadable;
+export default (BoardsHome, Board, Thread);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,6 +4,7 @@ import auth from './modules/auth';
 import notifs from './modules/notifs';
 import info from './modules/info';
 import boards from './modules/boards';
+import streams from './modules/streams';
 import profile from './modules/profile';
 
 export default function createReducers(asyncReducers) {
@@ -13,6 +14,7 @@ export default function createReducers(asyncReducers) {
     notifs,
     auth,
     boards,
+    streams,
     profile,
     info,
     ...asyncReducers
